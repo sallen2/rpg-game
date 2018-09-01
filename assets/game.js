22b288c1f7341e02d635daf41ca622d2833aec87
Stephan Allen <sallen202009@gmail.com>
2018-09-01 00:48:47 -0400

    added jQuery

--- assets/game.js
+++ assets/game.js
@@ -4,22 +4,22 @@ const playerMakup = {
         this.attackPower = attackPower;
         this.counterAttackPower = counterAttackPower;
         return this;
-    },
-    playerName: ""
+    }
 }
 
-const rpg = {
+const player = {
     luke: Object.create(playerMakup).init(60, 4, 10),
     darkVader: Object.create(playerMakup).init(300, 4, 10),
     yoda: Object.create(playerMakup).init(230, 50, 10),
     attack: function(playerAttack, enemyAttack){
-        document.getElementById("attack").addEventListener("click",function(){
+        $("#attack").on("click",function(){
             alert("I have attacked you!!!");
         });
-    }
+    },
+
 }
 
-rpg.attack();
+player.attack();
 
 
 
